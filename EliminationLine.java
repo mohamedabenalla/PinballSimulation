6e7af53fc288de0e64f71b70b0228666d9494db2
Mohamed Benalla <60680513+mohamedabenalla@users.noreply.github.com>
2021-07-25 22:22:48 -0400

    Delete EliminationLine.java

--- EliminationLine.java
+++
@@ -1,9 +0,0 @@
-
-public class EliminationLine extends Line {
-	//line at bottom of screen to determine if ball is eliminated
-	public EliminationLine() {
-		super(0, 800, 600, 800);
-		buffer = 4;
-	}
-
-}
